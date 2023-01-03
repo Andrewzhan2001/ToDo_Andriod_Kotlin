e502102b232d13a5575a9cc208d4fc3c352c3d0d
Tianyi.Zhan <tianyi.zhan@outlook.com>
2023-01-03 20:19:42 +0800

    add app bar for creating new task

---
+++ app/src/main/java/com/example/to_doapp/ui/screens/task/TaskAppBar.kt
@@ -0,0 +1,70 @@
+package com.example.to_doapp.ui.screens.task
+
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.runtime.*
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.to_doapp.R
+import com.example.to_doapp.ui.theme.topAppBarBackgroundColor
+import com.example.to_doapp.ui.theme.topAppBarContentColor
+import com.example.to_doapp.util.Action
+
+@Composable
+fun TaskAppBar( navigateToListScreen: (Action) -> Unit) {
+
+    NewTaskAppBar(navigateToListScreen = navigateToListScreen)
+
+}
+
+@Composable
+fun NewTaskAppBar(navigateToListScreen: (Action) -> Unit) {
+    TopAppBar(
+        navigationIcon = {
+            BackAction(onBackClicked = navigateToListScreen)
+        },
+        title = {
+            Text(
+                text = stringResource(id = R.string.add_task),
+                color = MaterialTheme.colors.topAppBarContentColor
+            )
+        },
+        backgroundColor = MaterialTheme.colors.topAppBarBackgroundColor,
+        actions = {
+            AddAction(onAddClicked = navigateToListScreen)
+        }
+    )
+}
+
+@Composable
+fun BackAction(onBackClicked: (Action) -> Unit) {
+    // back to list screen with no action
+    IconButton(onClick = { onBackClicked(Action.NO_ACTION) }) {
+        Icon(
+            imageVector = Icons.Filled.ArrowBack,
+            contentDescription = stringResource(id = R.string.back_arrow),
+            tint = MaterialTheme.colors.topAppBarContentColor
+        )
+    }
+}
+
+@Composable
+fun AddAction(onAddClicked: (Action) -> Unit) {
+    // back to list scrren and add the task
+    IconButton(onClick = { onAddClicked(Action.ADD) }) {
+        Icon(
+            imageVector = Icons.Filled.Check,
+            contentDescription = stringResource(id = R.string.add_task),
+            tint = MaterialTheme.colors.topAppBarContentColor
+        )
+    }
+}
+
+@Composable
+@Preview
+private fun NewTaskAppBarPreview() {
+    NewTaskAppBar(navigateToListScreen = {})
+}
+
